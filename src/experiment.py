518b16aa2a5125b17a2a965209ec78935e27346b
Catalin <catalinghitun0@gmail.com>
2025-12-05 13:32:32 +0100

    droput bn and more hyperparameters

--- src/experiment.py
+++ src/experiment.py
@@ -1,10 +1,7 @@
 import hydra
 from omegaconf import DictConfig  
 import lightning as lit
-from net.base import ConvAE, LSTMClassifier
-from lightning.pytorch import Trainer
-from lightning.pytorch.callbacks.early_stopping import EarlyStopping
-from lightning.pytorch.callbacks.model_checkpoint import ModelCheckpoint
+from net.base import Net
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader, random_split
 import wandb
@@ -20,54 +17,35 @@ import os
 @hydra.main(config_path="../cfg", config_name="base", version_base=None)
 def main(cfg: DictConfig):
     lit.seed_everything(cfg.seed) 
-    wandb_logger = WandbLogger(**cfg.wandb)
+    wandb_logger = WandbLogger(**cfg.wandb)  
+    model = Net(cfg.net)
+
     train_loader, val_loader, test_loader = load_har(**cfg.dataset)
-    conv_ae = ConvAE(cfg.net)
 
-    ae_checkpoint = ModelCheckpoint(
-        monitor="val_loss",
-        mode="min",
-        save_top_k=1,
-        filename="convAE-{epoch:02d}-{val_loss:.4f}"
-    )
-    ae_earlystop = EarlyStopping(monitor="val_loss", patience=15, mode="min")
+    trainer = lit.Trainer(logger=wandb_logger, callbacks=[
+        cb.EarlyStopping(
+            monitor="val_loss",
+            patience=15,
+            verbose=True,
+            mode="min", min_delta=1e-3
+        ), cb.ModelCheckpoint(
+            monitor="val_acc",
+             mode="max", 
+             save_top_k=1, 
+            filename="{epoch}-{val_acc:.4f}"),], 
+             **cfg.trainer)
 
-    ae_trainer = Trainer(
-        logger=wandb_logger,
-        callbacks=[ae_checkpoint, ae_earlystop],
-        **cfg.trainer
-    )
-
-    ae_trainer.fit(conv_ae, train_loader, val_loader)
-
-    best_encoder_path = ae_checkpoint.best_model_path
-    conv_ae = ConvAE.load_from_checkpoint(best_encoder_path, cfg=cfg.net)
-    pretrained_encoder = conv_ae.encoder
-
-    lstm_model = LSTMClassifier(cfg.net, pretrained_encoder)
-
-    lstm_checkpoint = ModelCheckpoint(
-        monitor="val_acc",
-        mode="max",
-        save_top_k=1,
-        filename="LSTM-{epoch:02d}-{val_acc:.4f}"
-    )
-    lstm_earlystop = EarlyStopping(monitor="val_acc", patience=15, mode="max")
-
-    lstm_trainer = Trainer(
-        logger=wandb_logger,
-        callbacks=[lstm_checkpoint, lstm_earlystop],
-        **cfg.trainer
-    )
-
-    lstm_trainer.fit(lstm_model, train_loader, val_loader)
-    best_lstm_path = lstm_checkpoint.best_model_path
-    best_lstm_model = LSTMClassifier.load_from_checkpoint(best_lstm_path, pretrained_encoder=pretrained_encoder, cfg=cfg.net)
-    lstm_trainer.test(best_lstm_model, test_loader)
+    print('Training...')
+    trainer.fit(model, train_loader, val_loader)
+    
+    print('Testing...')
+    best_model_path = trainer.checkpoint_callback.best_model_path
+    best_model = Net.load_from_checkpoint(best_model_path, cfg=cfg.net)
+    trainer.test(best_model, test_loader)
     
     hyperparams_dict = OmegaConf.to_container(cfg, resolve=True)
     hyperparams_dict["info"] = {  
-        "num_params": get_num_params(best_lstm_model),
+        "num_params": get_num_params(model),
     }
     wandb_logger.log_hyperparams(hyperparams_dict)  
 
--- src/net/cnn.py
+++ src/net/cnn.py
@@ -1,54 +1,19 @@
 import torch.nn as nn
 
 class ConvBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3, pool_size=2, **_):
+    def __init__(self, in_channels, out_channels, kernel_size=3, pool_size=2, dropout=0.5):
         super(ConvBlock, self).__init__()
-        self.conv = nn.Conv1d(in_channels, out_channels, kernel_size=kernel_size, padding=1)
+        self.conv = nn.Conv1d(in_channels, out_channels, kernel_size=kernel_size, padding= kernel_size//2)
+        self.bn = nn.BatchNorm1d(out_channels)
         self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(dropout)
         self.pool = nn.MaxPool1d(kernel_size=pool_size, stride=2)
 
     def forward(self, x):
         x = self.conv(x)
+        x = self.bn(x)
         x = self.relu(x)
+        x = self.dropout(x)
         x = self.pool(x)
         return x
 
-
-class DeconvBlock1D(nn.Module):
-    """
-    Deconvolution block for 1D signals:
-    ConvTranspose1d → (BatchNorm1d optional) → ReLU
-    """
-
-    def __init__(
-        self,
-        in_channels,
-        out_channels,
-        kernel_size=3,
-        stride=2,
-        padding=1,
-        output_padding=1,
-        use_batchnorm=True,
-    ):
-        super().__init__()
-
-        layers = [
-            nn.ConvTranspose1d(
-                in_channels=in_channels,
-                out_channels=out_channels,
-                kernel_size=kernel_size,
-                stride=stride,
-                padding=padding,
-                output_padding=output_padding,
-            )
-        ]
-
-        if use_batchnorm:
-            layers.append(nn.BatchNorm1d(out_channels))
-
-        layers.append(nn.ReLU(inplace=True))
-
-        self.net = nn.Sequential(*layers)
-
-    def forward(self, x):
-        return self.net(x)
